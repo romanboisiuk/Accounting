cb549eb8e20bce89bd4137c746cd68711c98d954
roman.boisiuk@gmail.com <roman.boisiuk@gmail.com>
2016-09-09 16:12:06 +0300

    add students urls,groups urls

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'students'
+    'students',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from students import views
 
 urlpatterns = [
+	# Students urls
+    url(r'^$', views.students_list, name='home'),
+    url(r'^students/add/$', views.students_add, name="students_add"),
+    url(r'^students/(?P<sid>\d+)/edit/$', views.students_edit, name="students_edit"),
+    url(r'^students/(?P<sid>\d+)/delete/$', views.students_delete, name="students_delete"),
+
+    # Groups urls
+    url(r'^groups/$', views.groups_list, name='groups'),
+    url(r'^groups/add/$', views.groups_add, name="groups_add"),
+    url(r'^groups/(?P<gid>\d+)/edit/$', views.groups_edit, name="groups_edit"),
+    url(r'^groups/(?P<gid>\d+)/delete/$', views.groups_delete, name="groups_delete"),
     url(r'^admin/', admin.site.urls),
 ]
