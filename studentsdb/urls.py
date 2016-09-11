c44b642732b0f0fea5db8818baa2e5058baacf96
roman.boisiuk@gmail.com <roman.boisiuk@gmail.com>
2016-09-11 21:15:48 +0300

    add teg {% for %}

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -20,9 +20,9 @@ from students import views
 urlpatterns = [
 	# Students urls
     url(r'^$', views.students_list, name='home'),
-    url(r'^students/add/$', views.students_add, name="students_add"),
-    url(r'^students/(?P<sid>\d+)/edit/$', views.students_edit, name="students_edit"),
-    url(r'^students/(?P<sid>\d+)/delete/$', views.students_delete, name="students_delete"),
+    url(r'^students/add/$', views.students_add, name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', views.students_edit, name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', views.students_delete, name='students_delete'),
 
     # Groups urls
     url(r'^groups/$', views.groups_list, name='groups'),
