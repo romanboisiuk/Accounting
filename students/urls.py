13a87be0604603e51dfb1595b4a0c3311d45c180
roman.boisiuk@gmail.com <roman.boisiuk@gmail.com>
2016-10-10 16:03:39 +0300

    create app urls

---
+++ students/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import  url
+from django.contrib import admin
+from views import students, groups, journal
+
+
+urlpatterns = [ 
+	# Students urls
+    url(r'^$', students.students_list, name='home'),
+    url(r'^students/add/$', students.students_add, name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', students.students_edit, name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', students.students_delete, name='students_delete'),
+
+  # Groups urls
+    url(r'^groups/$', groups.groups_list, name='groups'),
+    url(r'^groups/add/$', groups.groups_add, name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', groups.groups_edit, name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$', groups.groups_delete, name='groups_delete'),
+    
+    # Journal  urls
+  	url(r'^journal/$', journal.journal, name='journal'),
+]
+
