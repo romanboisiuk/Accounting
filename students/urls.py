157c9cc3964f1155f90c72a80003b37029e7dbe5
roman.boisiuk@gmail.com <roman.boisiuk@gmail.com>
2016-11-02 14:48:23 +0200

    create CBV for students and groups

--- students/urls.py
+++ students/urls.py
@@ -1,27 +1,26 @@
 from django.conf.urls import  url
 from django.contrib import admin
-from views import  journal 
-from views import  groups
-from views import  contact_admin
-from views import  students
+from views import journal, groups, contact_admin, students
+from views.students import StudentUpdateView, StudentDeleteView, StudentAddView
+from views.groups import GroupDeleteView, GroupUpdateView, GroupAddView
 
 urlpatterns = [ 
 	# Students urls
     url(r'^$', students.students_list, name='home'),
-    url(r'^students/add/$', students.students_add, name='students_add'),
-    url(r'^students/(?P<sid>\d+)/edit/$', students.students_edit, name='students_edit'),
-    url(r'^students/(?P<sid>\d+)/delete/$', students.students_delete, name='students_delete'),
+    url(r'^students/add/$', StudentAddView.as_view(), name='students_add'),
+    url(r'^students/(?P<pk>\d+)/edit/$', StudentUpdateView.as_view(), name='students_edit'),
+    url(r'^students/(?P<pk>\d+)/delete/$', StudentDeleteView.as_view(), name='students_delete'),
 
   # Groups urls
     url(r'^groups/$', groups.groups_list, name='groups'),
-    url(r'^groups/add/$', groups.groups_add, name='groups_add'),
-    url(r'^groups/(?P<gid>\d+)/edit/$', groups.groups_edit, name='groups_edit'),
-    url(r'^groups/(?P<gid>\d+)/delete/$', groups.groups_delete, name='groups_delete'),
+    url(r'^groups/add/$', GroupAddView.as_view(), name='groups_add'),
+    url(r'^groups/(?P<pk>\d+)/edit/$', GroupUpdateView.as_view(), name='groups_edit'),
+    url(r'^groups/(?P<pk>\d+)/delete/$', GroupDeleteView.as_view(), name='groups_delete'),
     
     # Journal  urls
   	url(r'^journal/$', journal.journal, name='journal'),
 
      # Contact Admin Form
-    url(r'^contact-admin/$', contact_admin.contact_admin, name='contact_admin'),
+    url(r'^contact_admin/$', contact_admin.contact_admin, name='contact_admin'),
 ]
 
