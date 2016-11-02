28b4d36ff8bb945022eebe49e7d048794f50d967
roman.boisiuk@gmail.com <roman.boisiuk@gmail.com>
2016-11-02 14:49:00 +0200

    fix models students

--- students/models/students.py
+++ students/models/students.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
 class Student(models.Model):
 	"""Student Model"""
 
@@ -41,15 +40,15 @@ class Student(models.Model):
 		blank = False,
 		verbose_name = u"Білет")
 
-	notes = models.TextField(
-		blank = True,
-		verbose_name = u"Додаткові нотатки")
-
 	student_group = models.ForeignKey('Group',
 		verbose_name=u"Група",
 		blank=False,
 		null=True,
 		on_delete=models.PROTECT)
-	
+
+	notes = models.TextField(
+		blank = True,
+		verbose_name = u"Додаткові нотатки")
+
 	def __unicode__(self):
 		return u"%s %s" % (self.first_name, self.last_name)	
