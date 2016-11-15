ed2ce71a0b02ba06529e0377d38de9c66ad34310
roman.boisiuk@gmail.com <roman.boisiuk@gmail.com>
2016-11-15 18:30:14 +0200

    add method ready for signals

--- students/apps.py
+++ students/apps.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
@@ -5,3 +6,7 @@ from django.apps import AppConfig
 
 class StudentsConfig(AppConfig):
     name = 'students'
+    verbose_name = 'База студентів'
+
+    def ready(self):
+    	from students import signals
